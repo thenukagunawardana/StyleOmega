36f8fc658e60b0b4153a22087a9d9c1692e903d9
thenukagunawardana <52916808+thenukagunawardana@users.noreply.github.com>
2020-01-18 15:02:39 +0530

    Commit 5

--- app/src/main/java/com/example/styleomege69/LoginActivity.java
+++ app/src/main/java/com/example/styleomege69/LoginActivity.java
@@ -144,7 +144,7 @@ public class LoginActivity extends AppCompatActivity
                                 Toast.makeText(LoginActivity.this, "Welcome Admin you are Logged In Successfully", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent= new Intent(LoginActivity.this,AdminAddNewProductActivity.class);
+                                Intent intent= new Intent(LoginActivity.this,AdminCategoryActivity.class);
                                 startActivity(intent);
                             }
                             else if (parentDBname.equals("Users"))
