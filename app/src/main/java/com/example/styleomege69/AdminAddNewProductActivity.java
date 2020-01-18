5f58cff616dd245644ebaa90a36f6fdf7fa2c804
thenukagunawardana <52916808+thenukagunawardana@users.noreply.github.com>
2020-01-18 15:57:11 +0530

    Commit 6

--- app/src/main/java/com/example/styleomege69/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/styleomege69/AdminAddNewProductActivity.java
@@ -3,17 +3,38 @@ package com.example.styleomege69;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.styleomege69.Prevalent.Prevalent;
 
-public class AdminAddNewProductActivity extends AppCompatActivity {
+public class AdminAddNewProductActivity extends AppCompatActivity
+{
+    private String CategoryName;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText ProductName_in,ProductDescription_in,ProductPrice_in;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
 
-        Toast.makeText(this, "Welcome..... ", Toast.LENGTH_SHORT).show();
+        CategoryName=getIntent().getExtras().get("category").toString();
+
+        AddNewProductButton=(Button)findViewById(R.id.add_new_product);
+        InputProductImage=(ImageView)findViewById(R.id.select_product_image);
+        ProductName_in=(EditText)findViewById(R.id.product_name);
+        ProductDescription_in=(EditText)findViewById(R.id.product_description);
+        ProductPrice_in=(EditText)findViewById(R.id.product_price);
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/styleomege69/AdminCategoryActivity.java
+++ app/src/main/java/com/example/styleomege69/AdminCategoryActivity.java
@@ -2,13 +2,59 @@ package com.example.styleomege69;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class AdminCategoryActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class AdminCategoryActivity extends AppCompatActivity
+{
+    private ImageView Mens,Womens,Shoes;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_catergory);
+
+        Mens=(ImageView) findViewById(R.id.men_add);
+        Womens=(ImageView)findViewById(R.id.female_add);
+        Shoes=(ImageView)findViewById(R.id.shoes_add);
+
+        Mens.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","men");
+                startActivity(intent);
+            }
+        });
+
+        Womens.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","women");
+                startActivity(intent);
+            }
+        });
+
+        Shoes.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent= new Intent(AdminCategoryActivity.this,AdminAddNewProductActivity.class);
+                intent.putExtra("category","shoes");
+                startActivity(intent);
+            }
+        });
     }
 }
