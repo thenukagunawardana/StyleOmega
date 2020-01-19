9f716f027728c823da5f4b45f8e135b31af65bde
thenukagunawardana <52916808+thenukagunawardana@users.noreply.github.com>
2020-01-19 14:33:18 +0530

    Commit 8

--- app/src/main/java/com/example/styleomege69/LoginActivity.java
+++ app/src/main/java/com/example/styleomege69/LoginActivity.java
@@ -153,6 +153,7 @@ public class LoginActivity extends AppCompatActivity
                                 loadingBar.dismiss();
 
                                 Intent intent= new Intent(LoginActivity.this,HomeActivity.class);
+                                Prevalent.currentOnlineUser=usersData;
                                 startActivity(intent);
                             }
                         }
--- app/src/main/java/com/example/styleomege69/Prevalent/Prevalent.java
+++ app/src/main/java/com/example/styleomege69/Prevalent/Prevalent.java
@@ -4,7 +4,7 @@ import com.example.styleomege69.Model.Users;
 
 public class Prevalent
 {
-    private static Users currentOnlineUser;
+    public static Users currentOnlineUser;
 
     public static final String UserNameKey="UserName";
     public static final String UserPasswordKey="UserPassword";
