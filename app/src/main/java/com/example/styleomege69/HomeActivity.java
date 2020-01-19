bea5e362b5023bea288ffb6f8090a97c43b7982e
thenukagunawardana <52916808+thenukagunawardana@users.noreply.github.com>
2020-01-19 15:54:34 +0530

    Commit 9

--- app/src/main/java/com/example/styleomege69/HomeActivity.java
+++ app/src/main/java/com/example/styleomege69/HomeActivity.java
@@ -3,10 +3,15 @@ package com.example.styleomege69;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.styleomege69.Model.Products;
 import com.example.styleomege69.Prevalent.Prevalent;
+import com.example.styleomege69.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -20,28 +25,39 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import io.paperdb.Paper;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
 {
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
 
-    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ProductsRef= FirebaseDatabase.getInstance().getReference().child("Products");
 
         Paper.init(this);
 
@@ -81,14 +97,47 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         userNameTextView.setText(Prevalent.currentOnlineUser.getName());
 
+        recyclerView=findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
+    }
 
-
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        FirebaseRecyclerOptions<Products> options=
+                new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(ProductsRef,Products.class)
                 .build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder>
+                adapter=new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i, @NonNull Products model)
+            {
+                productViewHolder.txtProductName.setText(model.getPname());
+                productViewHolder.txtProductDescription.setText(model.getDescription());
+                productViewHolder.txtProductPrice.setText("Price= "+model.getPrice()+"$");
+                Picasso.get().load(model.getImage()).into(productViewHolder.imageView);
+
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout,parent,false);
+                ProductViewHolder holder=new ProductViewHolder(view);
+                return holder;
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
     }
 
     @Override
